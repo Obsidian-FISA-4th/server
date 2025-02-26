6af82cceb794b03ac91e2f1aa4e9198ef2962cca
YoungjaeRo <ryj0330@naver.com>
2025-02-26 09:56:42 +0900

    feat : 프로젝트 세팅

---
+++ src/main/java/Obsidian/demo/ObisidianApplication.java
@@ -0,0 +1,13 @@
+package Obsidian.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObisidianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObisidianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Obsidian/demo/ObisidianApplicationTests.java
@@ -0,0 +1,13 @@
+package Obsidian.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObisidianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
