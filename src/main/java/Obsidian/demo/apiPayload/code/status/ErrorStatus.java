c9b53c783ce2c8f4505bf3394f08827a69a9fa4e
sengjun0624 <sengjun0624@naver.com>
2025-03-03 19:01:48 +0900

    feat: Markdown 관련 ErrorStatus 추가

--- src/main/java/Obsidian/demo/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/Obsidian/demo/apiPayload/code/status/ErrorStatus.java
@@ -18,9 +18,14 @@ public enum ErrorStatus implements BaseErrorCode {
 	_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
 	_BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."),
 	_UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
-	_FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+	_FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
-
+	MARKDOWN_FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "MARKDOWN404", "요청한 마크다운 파일을 찾을 수 없습니다."),
+	MARKDOWN_READ_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "MARKDOWN5001", "마크다운 파일을 읽는 도중 오류가 발생했습니다."),
+	MARKDOWN_CONVERT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "MARKDOWN5002", "마크다운을 HTML로 변환하는 도중 오류가 발생했습니다."),
+	HTML_SAVE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "MARKDOWN5003", "변환된 HTML 파일을 저장하는 도중 오류가 발생했습니다."),
+	DIRECTORY_CREATE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "MARKDOWN5004", "배포 경로 디렉토리를 생성하는 데 실패했습니다."),
+	PUBLIC_DIRECTORY_CLEAR_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "MARKDOWN5005", "배포 경로(publicPath)를 정리하는 도중 오류가 발생했습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
