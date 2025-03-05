f06f9bf0c1aab0199249061b17e36fab9f1b21c4
YoungjaeRo <ryj0330@naver.com>
2025-03-05 15:16:33 +0900

    feat : RedisConfig 구현

---
+++ src/main/java/Obsidian/demo/config/RedisConfig.java
@@ -0,0 +1,41 @@
+package Obsidian.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+	@Value("{spring.redis.host}")
+	private String redisHost;
+
+	@Value("{spring.redis.port}")
+	private int redisPort;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(redisHost, redisPort);
+		}
+
+	/**
+	 * RedisTemplate 을 통해서 JSON 데이터 직렬화와 역직렬화를 진행함
+	 */
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory());
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		return template;
+	}
+
+}
+
