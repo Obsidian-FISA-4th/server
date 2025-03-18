bcdfefe0811eddef9a631587e505e04ff3877360
sengjun0624 <sengjun0624@naver.com>
2025-03-18 20:37:50 +0900

    :sparkles: delete: mysql 삭제

--- src/main/java/Obsidian/demo/domain/common/BaseEntity.java
+++ src/main/java/Obsidian/demo/domain/common/BaseEntity.java
@@ -4,18 +4,14 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 /**
  * 기본 엔터티 extends 해서 사용하면 됨
  */
 
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+
 @Getter
 public abstract class BaseEntity {
 
